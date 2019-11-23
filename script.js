32aed9b4c8adb6ebabd0ad9290235011b4fc611b
mounirawy <amrnabil5700@gmail.com>
2019-11-23 04:01:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
-/**********
+// the call up girl data 
+const imgUrl1 = "img/happy.png";
+const imgUrl2 = "img/help.png";
+const imgUrl3 = "img/nice.png";
 
-  This Pen uses no libraries except fonts and should 
-  work on all modern browsers
-  
-  The answers are stored in the `questions` array
-  with the key `answer`. 
-  
+const dialogues = [
+    {imgsrc:imgUrl2,posLeft:"150px",posTop:"40px",
+    msg:'Hi! I am mona <br/> I am here to help you <br/>let us begin our journey.<br/> first let us know you.'},
+    {imgsrc:imgUrl3,posLeft:"150px",posTop:"30px",
+    msg:"Let us know your company better <br/> if you want to include your employees families <br/> just press yes."},
+    {imgsrc:imgUrl3,posLeft:"150px",posTop:"0px",
+    msg:"Please provide us for <br/> the range of the <br/>employees number. "},
+    {imgsrc:imgUrl3,posLeft:"150px",posTop:"30px",
+    msg:"Enter in egyptian pound <br/> please enter the annual coverage <br/>for every person and every dependent"},
+    {imgsrc:imgUrl3,posLeft:"160px",posTop:"50px",
+    msg:"If you have time to just<br/> upload our template full of ages <br/> press full data or for quick result <br/> jsut choose the average. "},
+    {imgsrc:imgUrl1,posLeft:"250px",posTop:"90px",
+    msg:"Provide us for the average <br/> note that it's just an expected value <br/> and it's not completly true.<br>you are about to complete."},
+    {imgsrc:imgUrl1,posLeft:"250px",posTop:"90px",
+    msg:"Download our tempate <br/> and just paste your employee ages <br/> then upload it again <br/>we are waiting for you.<br>you are about to complete."},   
+]
 
- **********/
-//default type question is text 
+
+// end of the call up girl data 
+
+
+
+
+
+//***important note that every specefic question has to be after options directly */
 
 var questions = [
-  {question:"What's your full name?",type:'text'}, 
+  {question:"Name",type:'text'}, 
   {question:"Do you want the plans to include the dependents?",type:"options",options:['Yes','No']},
 //   {question:"What's your email?", pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/},
-   {question:"Enter employee number?",type:'text'},
+   {question:"Employees number",type:'options',options:['10 - 14','15 - 19','20 - 49','+50']},
 //   {question:"Create your password", type: "password"}
-{question:"Enter your annual coverage maximum for every member.",type:'text'},
+{question:"Annual maximum coverage for every member.",type:'options',options:['25K - 30K','50K - 75K','100k - 150k']},
 {question:"",type:"options",options:['Enter average age values for estimated prices','Enter full data to get the actual price']},
-{question:"Enter the average age.",type:"specefic",type2:"text",circle:0,questionIndex:4}, //means show this question for people who choose the 0 index option at question index 4
+{question:"Average age.",type:"specefic",type2:"text",circle:0,questionIndex:4}, //means show this question for people who choose the 0 index option at question index 4
 {question:"Upload the template",type:"specefic",type2:"fileupload",circle:1,questionIndex:4}
 ]
 
@@ -26,7 +45,6 @@ var questions = [
   do something after the questions have been answered
 */
 var onComplete = function() {
-
     var h1 = document.createElement('h1')
     h1.appendChild(document.createTextNode('Thanks ' + questions[0].answer + ', We will email you.'))
     setTimeout(function() {
@@ -51,7 +69,8 @@ var onComplete = function() {
 
     forwardButton.addEventListener('click', validate)
     inputField.addEventListener('keyup', function(e) {
-        transform(0, 0) // ie hack to redraw
+        inputField.style.color = 'black';
+        transform(0, 0); // ie hack to redraw
         if (e.keyCode == 13) validate()
     })
 
@@ -191,6 +210,20 @@ var onComplete = function() {
 
         inputContainer.style.opacity = 1
 
+
+        // show up call up girl 
+        msg.innerHTML = dialogues[position].msg;
+
+        person.innerHTML = `<img src=${dialogues[position].imgsrc}/>`;
+
+        callupContainer.style.top = dialogues[position].posTop;
+        callupContainer.style.left = dialogues[position].posLeft;
+
+
+        
+        callupContainer.style.display = 'block';
+        // end of show up call up girl 
+
     }
 
     // when submitting the current question
@@ -257,7 +290,10 @@ var onComplete = function() {
         }
 
 
-        
+
+        // hide call up girl 
+        callupContainer.style.display = '';
+
 
         inputContainer.style.opacity = 0
         inputLabel.style.marginLeft = 0
@@ -272,6 +308,8 @@ var onComplete = function() {
         forwardButton.style.display = "";
          inputProgress.style.display = "";
         inputField.style.display = "";
+
+
 
         inputProgress.style.transition = ''
         inputProgress.style.width = '100%'
